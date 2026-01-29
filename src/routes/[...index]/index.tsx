d7a662895175ae59cf87f9b0cef10a3aaf2f84f7
MarioLopez <malr@thinkcares.com>
2026-01-29 10:53:15 -0700

    fist-comit

--- src/routes/[...index]/index.tsx
+++ src/routes/[...index]/index.tsx
@@ -1,25 +1,32 @@
-import { component$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-// Cambiamos RenderContent por Content
-import { fetchOneEntry, Content } from '@builder.io/sdk-qwik';
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { fetchOneEntry, Content } from "@builder.io/sdk-qwik";
 
-export const BUILDER_PUBLIC_API_KEY = '611cce7cd17f43e19dcd4d0e8298a0ab'; 
-export const BUILDER_MODEL = 'page';
+
+export const BUILDER_PUBLIC_API_KEY = "611cce7cd17f43e19dcd4d0e8298a0ab";
+export const BUILDER_MODEL = "page";
 
 export const useBuilderContent = routeLoader$(async ({ url }) => {
-  return await fetchOneEntry({
+ 
+  const content = await fetchOneEntry({
     model: BUILDER_MODEL,
     apiKey: BUILDER_PUBLIC_API_KEY,
     userAttributes: {
-      urlPath: url.pathname || '/',
+      urlPath: url.pathname || "/",
     },
   });
+
+  return content;
 });
 
 export default component$(() => {
   const builderContent = useBuilderContent();
 
-  // Usamos el componente <Content />
+  
+  if (!builderContent.value) {
+    return <div>No se encontró contenido en Builder.io para esta ruta.</div>;
+  }
+
   return (
     <Content
       model={BUILDER_MODEL}
